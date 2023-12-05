cc034735f08d208185051c2016eccbb1aac2b439
coder teddi <greg.choww@gmail.com>
2023-12-05 10:12:17 -0800

    updating 404.js data.Status

--- errors/404.js
+++ errors/404.js
@@ -22,7 +22,7 @@ function checkStatus() {
 fetch('https://aht00u6882.execute-api.us-east-1.amazonaws.com/getStatus/') //get last time via lambda function
     .then(response => response.json())
     .then((data) => {
-        if (data == 'Up') {
+        if (data.Status == 'Up') {
             document.getElementById("status").innerHTML = 'www.gregchow.net is currently: <span class="status-up">up</span>';
         } else {
             document.getElementById("status").innerHTML = 'www.gregchow.net is currently: <span class="status-down">down</span>';
